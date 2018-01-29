d09bc36223989ce769d848839ad7d389057a7065
Maher <Bouidani@Fayezs-MacBook-Pro.local>
2018-01-29 10:24:42 -0800

    your commit message here

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ import os
 import requests
 from apiclient import discovery
 
-TYPE_KEY_MAP = {
-    "1": ["dest", "identifier", "role", "type", "verkey"],
-    "101": ["data", "identifier", "reqId", "signature", "txnTime"],
-    "102": [
-        "data",
-        "identifier",
-        "ref",
-        "reqId",
-        "signature",
-        "signature_type",
-        "txnTime"
-    ]
-}
+# TYPE_KEY_MAP = {
+#    "1": ["dest", "identifier", "role", "type", "verkey"],
+ #   "101": ["data", "identifier", "reqId", "signature", "txnTime"],
+  #  "102": [
+   #     "data",
+   #    "identifier",
+    #    "ref",
+     #   "reqId",
+      #  "signature",
+       # "signature_type",
+        # "txnTime"
+   # ]
+# }
 
+HEADER_MAP= { 
+    "1": ["Label", "Type", "Tags", "Description"],
+    "102": ["Label", "Schema", "Claim Definition", "ref", "reqId"]
+}
 
 def update_sheets():
     # spreadsheetId = '13rFVDdfoJOLipB6upo46O9_QyDBWrhemexUo34LuX3A'
@@ -36,9 +40,9 @@ def update_sheets():
         'sheets', 'v4', http=http_auth, discoveryServiceUrl=discoveryUrl)
 
     # Write headers
-    for record_type in TYPE_KEY_MAP:
+    for record_type in HEADER_MAP:
         body = {'values': [
-            TYPE_KEY_MAP[record_type]
+            HEADER_MAP[record_type]
         ]}
 
         service.spreadsheets().values().update(
@@ -49,38 +53,124 @@ def update_sheets():
 
     # Get ledger data from BCOVRIN
     ledger_response = requests.get('http://138.197.170.136/ledger/domain')
+    counter_sheet_1 = 2
+    counter_sheet_2 = 2
+    #content = ledger_entry [2]
     for line in ledger_response.text.split('\n'):
         try:
             ledger_entry = json.loads(line)
         except json.decoder.JSONDecodeError:
             continue
 
-        sequence_number = ledger_entry[0]
+       # sequence_number = ledger_entry[0]
         content = ledger_entry[1]
 
-        body = {
-            'values': [[
-                # str(item[1]) for item in sorted(content.items())
-                str(content[key]) if key in content else ""
-                for key in TYPE_KEY_MAP[content['type']]
-            ]]
-        }
+        # Get the type
+        entity_type = content['type']
 
-        print('-Sending entry to Google Sheets-')
-        print('Range: Type%s!%s:%s' % (
-            content['type'], sequence_number, sequence_number))
-        print('Body: ' + str(body))
+        # Handle each type differently
+        if entity_type == "1":
+            # This ledger item is an identity
 
-        # Write ledger entries
-        service.spreadsheets().values().update(
-          spreadsheetId=spreadsheetId,
-          range='Type%s!%s:%s' % (
-            content['type'], sequence_number+1, sequence_number+1),
-          body=body,
-          valueInputOption="RAW").execute()
+            # For each of the attributes in this data type,
+            # extract the value into a variable. If the attribute
+            # isn't found in the data, use "" instead so that
+            # program doesn't exit with an error
+
+            Label = content['dest'] if 'dest' in content else ""
+            Type = content['role'] if 'role' in content else ""
+            Description = content['verkey'] if 'verkey' in content else ""
+            Tags = content['identifier'] if 'identifier' in content else ""
+
+            # We can transform the data as needed here. For example, if we wanted to
+            # change the role number into a human readable role name we could do:
+
+            # This should probably be at the top of the file if you use this.
+            ROLE_MAP = {
+                "0": "Trustee",
+                "2": "Steward",
+                "101": "Trust Anchor"
+            }
+
+            
+            role_name = ROLE_MAP[Type] if Type in ROLE_MAP else "No Role"
+           
+            # We build a row in the spreadsheet. We can have as many columns as we want
+            # for each row. Each entry in the array is a new column.
+
+            # Here we can use our transformed data and create new rows as needed!
+            row = [Label, role_name, Tags, Description]
+
+            body = {'values': [row]}
+
+            # Write ledger entries
+            print('print sequence number_1 ' + str(counter_sheet_1))
+            service.spreadsheets().values().update(
+            spreadsheetId=spreadsheetId,
+            range='Type%s!%s:%s' % (
+                content['type'], counter_sheet_1, counter_sheet_1),
+            body=body,
+            valueInputOption="RAW").execute()
+            counter_sheet_1 += 1 
+
+        #elif entity_type == "101":
+
+        #Label = content['identifier'] if 'identifier' in content else ""
+        #Schema = content['data'] if 'data' in content else ""
+
+        #row =[Label, Schema]  
+
+        elif entity_type == "102": 
+            
+            ClaimDef = content['signature'] if 'signature' in content else "" 
+            Ref = content['ref'] if 'ref' in content else ""
+            reqId = content['reqId'] if 'reqId' in content else ""
+                    
+            for line in ledger_response.text.split('\n'):
+                try:
+                    ledger_entry = json.loads(line)
+                except json.decoder.JSONDecodeError:
+                    continue
+
+                related_sequence_number = ledger_entry[0]
+
+                if related_sequence_number == Ref:
+                    related_content = ledger_entry[1]
+
+                    Label = related_content['identifier'] if 'identifier' in related_content else ""
+                    Schema = related_content['data'] if 'data' in related_content else ""
+                    
+            row = [str(Label), str(Schema), str(ClaimDef), str(Ref), str(reqId)]
+            body = {'values': [row]}
+            
+            # Write ledger entries
+            print('print sequence number_2 ' + str(counter_sheet_2))
+            service.spreadsheets().values().update(
+             spreadsheetId=spreadsheetId,
+            range='Type%s!%s:%s' % (
+                content['type'], counter_sheet_2, counter_sheet_2),
+            body=body,
+            valueInputOption="RAW").execute()
+
+            counter_sheet_2 += 1 
+
+            #legal_name = ...
+            #address = ...
+            #combined = 'Legal name: ' + legal_name + ' Address: ' + address
+            #example = legal_name + ' || ' + address
+            # We build the expected request format using the row above
+       
 
         # Avoid rate limiting
         time.sleep(0.2)
+
+    #claim_response = requests.get('https://django-devex-von-dev.pathfinder.gov.bc.ca/api/v1/verifiableclaims')
+    #claims = claim_response.json()
+
+    #for claim in claims:
+        
+        # do stuff
+        #pass
 
 
 def main():
